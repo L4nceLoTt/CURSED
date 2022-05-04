db4b4b16c6658ff92a6cdcc52b745f97f1cb1836
Никита Хандрико <nikh.17@mail.ru>
2022-05-04 12:51:10 +0300

    upd

--- CLIENT/CLIENT.cpp
+++ CLIENT/CLIENT.cpp
@@ -15,6 +15,41 @@
 
 using namespace std;
 
+Menu RegMenu, UserMenu, AdminMenu;
+Menu* MenuPtr = &RegMenu;
+
+class User {
+	bool isAdmin = false;
+	bool isSmth = false;
+
+public:
+	void setReg(bool status, bool reg) {
+		isAdmin = status;
+		isSmth = reg;
+	}
+	bool GetStatus() {
+		return isAdmin;
+	}
+	bool GetSmth() {
+		return isSmth;
+	}
+};
+
+User user;
+
+void Registr() {
+	string choose;
+	cout << "Admin? ";
+	cin >> choose;
+
+	if (choose == "да") user.setReg(true, true);
+	else user.setReg(false, true);
+}
+
+void Unreg() {
+	user.setReg(false, false);
+}
+
 
 void main() {
 	WORD wVersionRequested;
@@ -30,31 +65,29 @@ void main() {
 	dest_addr.sin_port = htons(1280);
 	dest_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	connect(s, (sockaddr*)&dest_addr, sizeof(dest_addr));
-	
-	Menu menu;
 
-	menu.CreateMenu(3, "Соси", "Залупу", "Ленина");
+	RegMenu.CreateMenu(1, "Войти"); 
 	{
-
+		RegMenu.function[0] = Registr;
 	}
+	UserMenu.CreateMenu(2, "Пользователь", "Выйти");
+	{
+		UserMenu.function[1] = Unreg;
+	}
+	AdminMenu.CreateMenu(2, "Админ", "Выйти");
+	{
+		AdminMenu.function[1] = Unreg;
+	}
+	
 
 	bool running = true;
 	while (running) {
-		menu.ShowMenu();
-		menu.Navigation(&running);
-		if (menu.currentID == "2") {
-			char buf[100];
-			strcpy(buf, menu.currentID.c_str());
-			send(s, buf, sizeof(buf), 0);
-			*buf = '\0';
-			cout << "ДОШЛО до СЮДА";
-			if (recv(s, buf, sizeof(buf), 0) != 0)
-			{
-				system("cls");
-				cout << buf;
-				_getch();
-			}
-		}
+		if (!user.GetStatus() && user.GetSmth()) MenuPtr = &UserMenu;
+		else if (!user.GetStatus() && !user.GetSmth()) MenuPtr = &RegMenu;
+		else if (user.GetStatus() && user.GetSmth()) MenuPtr = &AdminMenu;
+
+		MenuPtr->ShowMenu();
+		MenuPtr->Navigation(&running);
 	}
 
 	closesocket(s);
